e6a19b04365cfe964894ec393e8253b0f5e29ec0
Paulhenry <paulhenrysaux@gmail.com>
2025-10-10 18:37:41 +0000

    commit

--- app/src/main/java/com/example/topoclimb/MainActivity.kt
+++ app/src/main/java/com/example/topoclimb/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TopoClimbTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Paulhenry",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
